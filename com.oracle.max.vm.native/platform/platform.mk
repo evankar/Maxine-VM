d4b480249ecccccbe0d4081df5f53fd5a9fd3f1d
Foivos Zakkak <foivos.zakkak@manchester.ac.uk>
2020-03-24 00:32:43 +0200

    [refactor] revert whitespace changes in platform.mk

--- com.oracle.max.vm.native/platform/platform.mk
+++ com.oracle.max.vm.native/platform/platform.mk
@@ -20,31 +20,40 @@
 # 2 along with this work; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
 #
+
 # Print verbose output only when MX_VERBOSE is set to y (i.e. mx -V command is used)
 ifneq ($(MX_VERBOSE), y)
     AT := @
     MAKE := $(MAKE) -s
 endif
+
 # Currently all Maxine target implementations are 64 bit.
 # This is where you might change that for the native code; TARGET_WORD_SIZE is interpreted in word.h
 # N.B. There are no doubt still assumptions in the code that host and target word size are the same.
 # These should be fixed.
+
 TARGET_WORD_SIZE := w64
+
 ifeq ($(LIB), hosted)
     TARGET := HOSTED
 endif
+
 ifeq ($(LIB), tele)
     TARGET := TELE
 endif
+
 ifeq ($(LIB), jvm)
     TARGET := SUBSTRATE
 endif
+
 ifeq ($(LIB), javatest)
     TARGET := SUBSTRATE
 endif
+
 ifneq ($(MAIN),)
     TARGET := LAUNCH
 endif
+
 # HOSTOS is the platform we are compiling on
 HOSTOS = $(shell uname -s)
 # TARGETOS is the platform we are compiling for (usually the same as HOSTOS)
@@ -66,12 +75,14 @@ endif # SunOS
 TARGETISA ?= $(HOSTISA)
 # Always use HOSTISA as the TARGETISA for libhosted
 ifeq ($(TARGET), HOSTED)
-    ifneq ($(TARGETISA), $(HOSTISA))
-        TARGETISA := $(HOSTISA)
-        CROSS_COMPILING := 1
-    endif
+	ifneq ($(TARGETISA), $(HOSTISA))
+	    TARGETISA := $(HOSTISA)
+	    CROSS_COMPILING := 1
+	endif
 endif
+
 DARWIN_RELEASE ?= $(shell echo $$(($$(uname -r | cut -d'.' -f 1) > 13 ? 1 : 0)))
+
 ifeq ($(TARGETOS),Darwin)
     OS := darwin
     DARWIN_GCC_MFLAG :=
@@ -96,6 +107,7 @@ ifeq ($(TARGETOS),Darwin)
        endif
     endif
 endif
+
 ifeq ($(TARGETOS),Linux)
     OS := linux
     ifeq ($(TARGETISA), x86_64)
@@ -116,6 +128,7 @@ ifeq ($(TARGETOS),Linux)
         TARGET_WORD_SIZE := w32
     endif
 endif
+
 ifeq ($(TARGETOS),SunOS)
     OS := solaris
     OTHER_CFLAGS := -KPIC  -DLP64
@@ -137,14 +150,17 @@ ifeq ($(TARGETOS),SunOS)
         PLATFORM := $(OS)-$(ISA)$(ARCH)
     endif
 endif
+
 ifeq ($(findstring CYGWIN,$(TARGETOS)),CYGWIN)
     OS := windows
     ISA := ia32
 endif
+
 # There are three variants for Maxine VE, owing to the 32/64 dom0 variants
 # MaxVE: 64 bit dom0, Inspector running in dom0, with libtele referencing 64-bit libguk/libxen*
 # MaxVE64H: Inspector running in domU, with 64 bit libtele not referencing libguk/libxen
 # MaxVE32T: 32 bit dom0, Inspector agent running in dom0, with 32 bit libtele referencing 32 bit libguk/libxen
+
 ifeq ($(TARGETOS),MaxVE)
     HYP := xen
     OS := maxve
@@ -153,6 +169,7 @@ ifeq ($(TARGETOS),MaxVE)
     ISA := amd64
     ARCH := amd64
 endif
+
 ifeq ($(TARGETOS),MaxVE64H)
     HYP := xen
     OS := maxve
@@ -161,6 +178,7 @@ ifeq ($(TARGETOS),MaxVE64H)
     ISA := amd64
     ARCH := amd64
 endif
+
 ifeq ($(TARGETOS),MaxVE32T)
     HYP := xen
     OS := maxve
@@ -169,35 +187,43 @@ ifeq ($(TARGETOS),MaxVE32T)
     ISA := amd64
     ARCH := amd64
 endif
+
 ifndef OS
     $(error unknown OS)
 endif
+
 ifndef ISA
     $(error unknown ISA)
 endif
+
 ifndef PLATFORM
     PLATFORM := $(OS)-$(ISA)
 endif
+
+
 # identify the JDK version (there are differences in how Maxine is built between JDK 7 and 8)
 ifdef JAVA_HOME
-    javacmd := $(JAVA_HOME)/bin/java
+	javacmd := $(JAVA_HOME)/bin/java
 else
-    javacmd := java
+	javacmd := java
 endif
 jdk_version := $(shell $(javacmd) -version 2>&1 | head -n 1)
 ifeq ($(findstring 1.7,$(jdk_version)),1.7)
-    JDK := -DJDK7
+	JDK := -DJDK7
 else
     ifeq ($(findstring 1.8,$(jdk_version)),1.8)
         JDK := -DJDK8
     endif
 endif
+
+
 # Always build libhosted wih gcc when cross-compiling
 ifeq ($(TARGET), HOSTED)
-    ifeq ($(CROSS_COMPILING), 1)
+	ifeq ($(CROSS_COMPILING), 1)
         CC := gcc
     endif
 endif
+
 ifeq ($(OS),darwin)
     ifneq "$(findstring def, $(origin CC))" ""
         # origin of CC is either undefined or default, so set it here
@@ -227,6 +253,7 @@ ifeq ($(OS),darwin)
     LIB_SUFFIX = .dylib
     JAVA_HOME ?= /Library/Java/JavaVirtualMachines/jdk1.8.0_151.jdk/Contents/Home
 endif
+
 ifeq ($(OS),linux)
     ifneq "$(findstring def, $(origin CC))" ""
         # origin of CC is either undefined or default, so set it here
@@ -263,6 +290,7 @@ ifeq ($(OS),linux)
     LIB_PREFIX = lib
     LIB_SUFFIX = .so
 endif
+
 ifeq ($(OS),solaris)
     ifneq "$(findstring def, $(origin CC))" ""
         # origin of CC is either undefined or default, so set it here
@@ -280,6 +308,7 @@ ifeq ($(OS),solaris)
     LIB_PREFIX = lib
     LIB_SUFFIX = .so
 endif
+
 ifeq ($(OS),windows)
     # determine predefined macros: touch foo.c; gcc -E -dD foo.c
     ifneq "$(findstring def, $(origin CC))" ""
@@ -296,6 +325,7 @@ ifeq ($(OS),windows)
     LIB_PREFIX =
     LIB_SUFFIX = .dll
 endif
+
 ifeq ($(OS),maxve)
     # assume Xen hypervisor
     ifeq ($(TARGET),TELE)
@@ -331,6 +361,7 @@ ifeq ($(OS),maxve)
     else
         AR = gar
     endif
+
     LINK_AR = $(AR) r $(LIB_PREFIX)$(LIB)$(LIBA_SUFFIX)
     ifeq ($(GUK),1)
         XG_ROOT = $(XEN_ROOT)/tools/debugger/gdbsx/xg
@@ -343,9 +374,12 @@ ifeq ($(OS),maxve)
         LINK_LIB = $(CC) -shared -lc -lm $(mf)
     endif
 endif
+
 ifndef JAVA_HOME
     ignore := $(error "Must set JAVA_HOME environment variable to your JDK home directory")
 endif
+
+
 ifeq ($(OS),maxve)
     # no maxve in your typical JAVA_HOME so have to use host
     ifeq ($(HOSTOS),Darwin)
@@ -366,6 +400,7 @@ ifeq ($(OS),maxve)
         endif
         CFLAGS += -I $(XEN_ROOT)/tools/debugger/gdbsx/xg
     endif
+
 else
     ifeq ($(OS),darwin)
         ifdef JDK
@@ -383,8 +418,10 @@ else
         JNI_H_PATH = $(wildcard $(JAVA_HOME)/include/jni.h)
     endif
 endif
+
 ifeq "$(JNI_H_PATH)" ""
     $(error Could not find path to jni.h in $(JAVA_HOME))
 endif
+
 C_DEPENDENCIES_FLAGS += $(JNI_INCLUDES) -DJNI_H_PATH=\"$(JNI_H_PATH)\"
 CFLAGS += $(JNI_INCLUDES) -DJNI_H_PATH=\"$(JNI_H_PATH)\"
